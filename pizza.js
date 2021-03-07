a2e990bde015af29158f0a2e9eecd8de921a8b1f
ilona <isaevaangelina@gmail.com>
2021-03-07 14:03:24 +0200

    Initial commit

---
+++ pizza.js
@@ -0,0 +1,5 @@
+function howBiggaMaPizza(blob, crust) {
+    const  exp = crust === "crispy" ? 3 * 1 : 3 * 5 //  1 and 5 deep pizza
+    return ( 4 * ( blob / 2 ) **3 / exp ) **0.5
+}
+//https://www.codewars.com/kata/5e9c06f95ea5b30033903194
